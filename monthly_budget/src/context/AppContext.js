5b274bf8b15d748d638521a3f0cbfbc9cd2c20a0
Pravesh Jain1 <pravesh.jain1@ibm.com>
2021-08-05 11:27:07 +0530

    Finished building basic react app

--- monthly_budget/src/context/AppContext.js
+++ monthly_budget/src/context/AppContext.js
@@ -1,5 +1,20 @@
+import React from 'react'
+
+
 const AppReducer = (state, action) => {
 	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
+			};
 		default:
 			return state;
 	}
@@ -14,10 +29,10 @@ const initialState = {
 	],
 };
 
-export const AppContext = createContext();
+export const AppContext = React.createContext();
 
 export const AppProvider = (props) => {
-	const [state, dispatch] = useReducer(AppReducer, initialState);
+	const [state, dispatch] = React.useReducer(AppReducer, initialState);
 
 	return (
 		<AppContext.Provider
