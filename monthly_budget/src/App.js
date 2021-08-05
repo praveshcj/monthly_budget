441681342c74e595f85ab26e85856eb929dc2219
Pravesh Jain <praveshjain500@gmail.com>
2021-08-05 12:37:42 +0530

    Nav Bar added

--- monthly_budget/src/App.js
+++ monthly_budget/src/App.js
@@ -1,42 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Budget from './components/Budget';
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import AddExpenseForm from './components/AddExpenseForm';
-import { AppProvider } from './context/AppContext';
+import Expenses from './components/Expenses/Expenses';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 const App = () => {
 	return (
-		<AppProvider>
-			<div className='container'>
-				<h1 className='mt-3'>My Budget Planner</h1>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<Budget />
-					</div>
-					<div className='col-sm'>
-						<Remaining />
-					</div>
-					<div className='col-sm'>
-						<ExpenseTotal />
-					</div>
-				</div>
-				<h3 className='mt-3'>Expenses</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<ExpenseList />
-					</div>
-				</div>
-				<h3 className='mt-3'>Add Expense</h3>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<AddExpenseForm />
-					</div>
-				</div>
-			</div>
-		</AppProvider>
+    <Router>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="/">Dashboard</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/expenses">Expenses</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    <Switch>
+      <Route path='/expenses' component={Expenses}/>
+    </Switch>
+  </Router>
 	);
 };
 
